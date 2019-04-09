7239aa66ba8a346f3c8604ce8c68062a82b49362
Joseph <josephwow1@gamil.com>
2019-04-09 12:11:25 -0300

    Updates

---
+++ precache-manifest.d1bf8c398c5337967f1e60ab64940711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/signin-signup-form/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "280d44c1dbdb566a599831852d4a40ca",
+    "url": "/signin-signup-form/static/media/bg-cover.280d44c1.jpeg"
+  },
+  {
+    "revision": "6e4e9dbedcbe2e2461bf",
+    "url": "/signin-signup-form/static/js/runtime~main.8b958cc4.js"
+  },
+  {
+    "revision": "b3898105ad9d219f3e17",
+    "url": "/signin-signup-form/static/js/main.e3322932.chunk.js"
+  },
+  {
+    "revision": "df27374e6491f8c37a75",
+    "url": "/signin-signup-form/static/js/2.0e0617fb.chunk.js"
+  },
+  {
+    "revision": "b3898105ad9d219f3e17",
+    "url": "/signin-signup-form/static/css/main.f9b533b9.chunk.css"
+  },
+  {
+    "revision": "f2308d5cc6dcf785d802cac88aae2751",
+    "url": "/signin-signup-form/index.html"
+  }
+];
